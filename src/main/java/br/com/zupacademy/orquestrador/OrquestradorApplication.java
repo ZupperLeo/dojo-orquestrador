49c420b52e94c8631381f32323280e757069217d
ThiagoZup <thiago.reis@zup.com.br>
2021-10-05 14:40:50 -0300

    setup

---
+++ src/main/java/br/com/zupacademy/orquestrador/OrquestradorApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.orquestrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrquestradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrquestradorApplication.class, args);
+	}
+
+}
